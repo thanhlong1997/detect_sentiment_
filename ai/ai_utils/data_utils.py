ed14c9d1f4bedbb43d92f508d0abdc3a437852e6
thanhlong1997 <33058432+thanhlong1997@users.noreply.github.com>
2018-09-21 15:27:09 +0700

    Update data_utils.py

--- ai/ai_utils/data_utils.py
+++ ai/ai_utils/data_utils.py
@@ -1,14 +1,13 @@
-    import pandas as pd
-    def load_excel_training_data(excel_file,excel_sheet):
-        df = pd.read_excel(excel_file, sheetname=excel_sheet, encoding="UTF-8")
-        training_data={}
-        for index in tags:
-            training_data[tags[index]]=[]
-
-        for item in df.index:
-            training_data[tags[df['label'][item]]].append(df['content'][item])
-        return training_data
-    def read_exel(exel_file,exel_sheet):
-        df=pd.read_excel(excel_file, sheetname=excel_sheet, encoding="UTF-8")
-        return df
+import pandas as pd
+def load_excel_training_data(excel_file,excel_sheet):
+    df = pd.read_excel(excel_file, sheetname=excel_sheet, encoding="UTF-8")
+    training_data={}
+    for index in tags:
+        training_data[tags[index]]=[]
+    for item in df.index:
+        training_data[tags[df['label'][item]]].append(df['content'][item])
+    return training_data
+def read_exel(exel_file,exel_sheet):
+    df=pd.read_excel(excel_file, sheetname=excel_sheet, encoding="UTF-8")
+    return df
         
